1cb1bf01d8ee29ce982b5a6e5a8994937acc9164
HILMI SARIOGLU <77903324+hilmisarioglu@users.noreply.github.com>
2025-02-01 21:17:02 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAzCD7HWpXS1-opJBMM6AmLIZJ6XLoUhC0",
-  authDomain: "fitness-app-e5190.firebaseapp.com",
-  projectId: "fitness-app-e5190",
-  storageBucket: "fitness-app-e5190.firebasestorage.app",
-  messagingSenderId: "533227712675",
-  appId: "1:533227712675:web:ed213e09ea447e4559cf16",
-  measurementId: "G-4KPJ9PEVRY"
+
 };
 
 // Initialize Firebase
